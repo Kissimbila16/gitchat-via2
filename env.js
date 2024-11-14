1c2ba095c093c56654d6be8098d2bdaf5564a12d
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-14 16:28:16 +0000

    feat: editei o env.js

--- env.js
+++ env.js
@@ -5,10 +5,10 @@ const keys= {
   DATABASE_LOCAL: "",
   DATABASE_PASSWORD: "",
 
-  DB_HOST: "localhost",
+  DB_HOST: "dpg-csr28laj1k6c73c5g4hg-a",
   DB_DATABASE: "themesbrand_chatvia_node",
-  DB_USERNAME: "root",
-  DB_PASSWORD: "",
+  DB_USERNAME: "themesbrand_chatvia_node_user",
+  DB_PASSWORD: "M8XUoiYvWQ191DZhiRkEFiuUjAj6E7bT",
 };
 
 module.exports = keys;
