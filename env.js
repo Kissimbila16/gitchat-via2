6b8bd983f35a255219dbcd62066dda95cf4686a1
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-14 00:50:47 +0000

    feat: iniciando o novo git chat

---
+++ env.js
@@ -0,0 +1,13 @@
+module.keys= {
+  NODE_ENV: "production",
+  PORT: "9090",
+  DATABASE: "",
+  DATABASE_LOCAL: "",
+  DATABASE_PASSWORD: "",
+
+  DB_HOST: "localhost",
+  DB_DATABASE: "themesbrand_chatvia_node",
+  DB_USERNAME: "root",
+  DB_PASSWORD: "",
+};
+
