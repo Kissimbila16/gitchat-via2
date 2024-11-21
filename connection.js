b04a52822a2d396fcd48a6784d0315eb07847781
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-21 15:13:46 +0000

    add a conexao com a base de dados

--- connection.js
+++ connection.js
@@ -4,9 +4,9 @@ const keys = require('./env');
 // configraration with env. 
 dotenv.config();
 var dbConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: keys.DB_HOST,
+  user: keys.DB_USERNAME,
+  password:keys.DB_PASSWORD,
   database: keys.DB_DATABASE,
   charset : 'utf8mb4'
 });
--- env.js
+++ env.js
@@ -5,10 +5,10 @@ const keys= {
   DATABASE_LOCAL: "",
   DATABASE_PASSWORD: "",
 
-  DB_HOST: "dpg-csr28laj1k6c73c5g4hg-a",
-  DB_DATABASE: "themesbrand_chatvia_node",
-  DB_USERNAME: "themesbrand_chatvia_node_user",
-  DB_PASSWORD: "M8XUoiYvWQ191DZhiRkEFiuUjAj6E7bT",
+  DB_HOST: "srv1596.hstgr.io",
+  DB_DATABASE: "u653461409_extra_date",
+  DB_USERNAME: "u653461409_extra_date",
+  DB_PASSWORD: "djK_2_extra_date",
 };
 
 module.exports = keys;
