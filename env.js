0d8b2549878e98a415f2db7c214b75cc075996b0
Domingos kissimbila <doncajoblack@gmail.com>
2024-11-30 22:35:55 +0000

    removi o captcha

--- env.js
+++ env.js
@@ -1,14 +1,14 @@
 const keys= {
-  NODE_ENV: "production",
-  PORT: "9090",
-  DATABASE: "",
-  DATABASE_LOCAL: "",
-  DATABASE_PASSWORD: "",
 
   DB_HOST: "srv1596.hstgr.io",
   DB_DATABASE: "u653461409_extra_date",
   DB_USERNAME: "u653461409_extra_date",
   DB_PASSWORD: "djK_2_extra_date",
+
+//   DB_HOST: "localhost",
+//   DB_DATABASE: "themesbrand_chatvia_node",
+//   DB_USERNAME: "root",
+//   DB_PASSWORD: "",
 };
 
 module.exports = keys;
